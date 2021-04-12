f335a7ea05818ee82630f14732e309d7c96475d3
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-04-12 17:41:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Add your code here
+// fetch("http://localhost:3000/dogs", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify(
+//   {dogName: "Sm",
+//     dogBreed: "Poodle"} ,
+//     )
+// }); 
+
+  
+const dom = document.querySelector("body")
+
+function submitData(name, email){
+
+    let toStringify = {
+        name,
+        email
+    }
+    
+    let jsonRequest = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(toStringify)
+    }
+
+    return fetch("http://localhost:3000/users", jsonRequest)
+    .then(response => response.json()) // Retrieve incoming data and convert to JSON
+    .then(obj => dom.innerHTML = obj.id) // Append ID to DOM
+    .catch(error => dom.innerHTML = error.message); // Catch failed post using catch
+
+}
+
+submitData("Steve-O", "s1@gmail")
